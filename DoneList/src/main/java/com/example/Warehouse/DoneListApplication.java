7f80d92a30b2c850d565395d6b90f9f913933622
Nelson <Nelson@DESKTOP-HS7BA3F>
2021-07-18 11:21:39 +0800

    Create Project

---
+++ DoneList/src/main/java/com/example/Warehouse/DoneListApplication.java
@@ -0,0 +1,13 @@
+package com.example.Warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoneListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoneListApplication.class, args);
+	}
+
+}
