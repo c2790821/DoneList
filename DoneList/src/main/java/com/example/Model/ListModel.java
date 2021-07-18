7b92d55aab2ff05d1eff5f2aa6baa58f49d60f48
Nelson <Nelson@DESKTOP-HS7BA3F>
2021-07-18 12:32:20 +0800

    Create ListModel

--- DoneList/src/main/java/com/example/Model/ListModel.java
+++ DoneList/src/main/java/com/example/Model/ListModel.java
@@ -1,5 +1,38 @@
 package com.example.Model;
 
-public class ListModel {
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ListModel {
+	String date;
+	String content;
+	
+	public ListModel() {
+		
+	}
+	
+	public ListModel(String content) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		this.date    = sdf.format(new Date());
+		this.content = content;
+	}
+	
+	public ListModel(String content, String date) {
+		this.date 	 = date;
+		this.content = content;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
