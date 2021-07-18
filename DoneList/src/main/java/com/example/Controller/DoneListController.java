020592a35fb02b20cd818164790b1f1e8e22cfd7
Nelson <Nelson@DESKTOP-HS7BA3F>
2021-07-18 13:06:23 +0800

    Edit DoneListController

--- DoneList/src/main/java/com/example/Controller/DoneListController.java
+++ DoneList/src/main/java/com/example/Controller/DoneListController.java
@@ -1,14 +1,29 @@
 package com.example.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.Model.ListModel;
+import com.example.Service.DoneListFindService;
 
 @Controller
 @RequestMapping("/API")
 public class DoneListController {
 	
+	
+	@Autowired
+	DoneListFindService doneListFindService;
+	
 	@RequestMapping("/DoneList")
-	public String doneList() {
+	public String doneList(Model model) {
+		ListModel listModel = new ListModel();
+		List<ListModel> listModels = doneListFindService.findAll(); 
+		model.addAttribute("listModel", listModel);
+		model.addAttribute("listModels" ,listModels);
 		return "DoneList";
 	}
 }
