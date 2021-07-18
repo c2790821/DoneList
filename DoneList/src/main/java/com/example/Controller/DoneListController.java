2ca2ed0b6ff563b6429bcee9ee889ed48c8f533b
Nelson <Nelson@DESKTOP-HS7BA3F>
2021-07-18 13:21:06 +0800

    Edit

--- DoneList/src/main/java/com/example/Controller/DoneListController.java
+++ DoneList/src/main/java/com/example/Controller/DoneListController.java
@@ -2,6 +2,8 @@ package com.example.Controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,12 +16,14 @@ import com.example.Service.DoneListFindService;
 @RequestMapping("/API")
 public class DoneListController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(DoneListController.class);
 	
 	@Autowired
 	DoneListFindService doneListFindService;
 	
 	@RequestMapping("/DoneList")
 	public String doneList(Model model) {
+		logger.info("Start doneList");
 		ListModel listModel = new ListModel();
 		List<ListModel> listModels = doneListFindService.findAll(); 
 		model.addAttribute("listModel", listModel);
