63707c3cd925ea8d3506b66a13bbd8b543358299
Nelson <Nelson@DESKTOP-HS7BA3F>
2021-07-18 12:48:39 +0800

    Create DoneListDao

---
+++ DoneList/src/main/java/com/example/Dao/DoneListDao.java
@@ -0,0 +1,34 @@
+package com.example.Dao;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.Model.ListModel;
+
+@Repository
+public class DoneListDao {
+	
+	private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+	
+	public void insert(ListModel model) {
+		String table = "Done_List";
+		String query = "INSER INTO Done_List(CONTENT,DATE)VALUES(?,?)";
+		jdbcTemplate.update(query, model.getContent(), model.getDate());
+	}
+	
+	public List<ListModel> findAll(){
+		LinkedList<ListModel> doneLists = new LinkedList<ListModel>();
+		String query = "SELECT * FROM Done_List";
+		List<Map<String, Object>> results = (List<Map<String, Object>>) jdbcTemplate.queryForList(query);
+		for(Map<String, Object> result: results) {
+			String content = (String) result.get("CONTENT");
+			String date    = (String) result.get("DATE");
+			doneLists.addFirst(new ListModel(content, date));
+		}
+		return doneLists;
+	}
+}
