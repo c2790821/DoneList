eab3314a13166c67deefc52cd2f89ca4e7010b3b
Nelson <Nelson@DESKTOP-HS7BA3F>
2021-07-25 15:47:58 +0800

    增加test

--- DoneList/src/test/java/com/example/Warehouse/DoneListApplicationTests.java
+++ DoneList/src/test/java/com/example/Warehouse/DoneListApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.Warehouse;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.DoneList.Hello;
+
+@SpringBootTest(classes = Hello.class)
 class DoneListApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1,1);
 	}
 
 }
