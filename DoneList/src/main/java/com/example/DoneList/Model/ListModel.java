23c2ede000c43b543c0d0c9dbd946f55103bc6c1
Nelson <Nelson@DESKTOP-HS7BA3F>
2021-07-25 15:47:30 +0800

    新增id

--- DoneList/src/main/java/com/example/DoneList/Model/ListModel.java
+++ DoneList/src/main/java/com/example/DoneList/Model/ListModel.java
@@ -1,38 +1,61 @@
 package com.example.DoneList.Model;
 
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ListModel {
-	String date;
-	String content;
-	
+	private Integer id;
+	private String date;
+	private String content;
+
 	public ListModel() {
-		
+
 	}
-	
+
 	public ListModel(String content) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		this.date    = sdf.format(new Date());
+		this.date = sdf.format(new Date());
+		this.content = content;
+	}
+
+	public ListModel(String content, String date) {
+		this.date = date;
 		this.content = content;
 	}
 	
-	public ListModel(String content, String date) {
-		this.date 	 = date;
+	public ListModel(Integer id, String content, String date) {
+		this.id = id;
+		this.date = date;
 		this.content = content;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
 	}
 	
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+	
+	@Override
+	public String toString() {
+		return this.id + this.date + this.content;
+	}
+
 }
